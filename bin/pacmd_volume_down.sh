8554857e72be1a8286eca0b23fd8bfb38256546b
Chris Templin <ctemplin@lockify.com>
2012-05-19 17:11:57 -0400

    Adding shell scripts to control pulseaudio volume.

---
+++ bin/pacmd_volume_down.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+CURVOLHEX=`pacmd dump | egrep 'set-sink-volume alsa_output.pci-0000_00_1b.0.analog-stereo' | cut -d ' ' -f 3`
+CURVOLDEC=`printf "%d" $CURVOLHEX`
+NEWVOLDEC=$[$CURVOLDEC - 3276]
+NEWVOLHEX=`printf "%x" $NEWVOLDEC`
+pacmd set-sink-volume alsa_output.pci-0000_00_1b.0.analog-stereo 0x$NEWVOLHEX  
